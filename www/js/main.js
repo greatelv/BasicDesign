6f3c47d7763100eb840f86c9d5e6c348ca8a3628
tkjeon <wjswjs2@gmail.com>
2014-08-24 21:31:10 +0900

    - 배치 파일 수정 및 메인 수정

--- www/js/main.js
+++ www/js/main.js
@@ -9,18 +9,19 @@ var main = (function(){
 	
 	var bindHandler = function(){
 		$( "#items .item" ).on("vclick", function(){
+			console.log('isTouchMoving  : '+isTouchMoving);
 			if(isTouchMoving){
 				isTouchMoving = false;
 				return false;
 			}
+
+			$(document).on('vmousemove', function(){
+				isTouchMoving = true;
+			});
 			
 			var item = $(this);
 			elem.items.children('.over').hide();
 			item.children('.over').fadeIn(200);
-		});
-		
-		$(document).on('vmousemove', function(){
-			isTouchMoving = true;
 		});
 		
 		$(document).on('scrollstop', function(){
