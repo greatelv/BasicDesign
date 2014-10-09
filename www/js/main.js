353ecf70622ff044688c35471d2f63d9c2c0864c
tkjeon <wjswjs2@gmail.com>
2014-10-09 16:43:06 +0900

    * cordova 카메라 연동 완료

--- www/js/main.js
+++ www/js/main.js
@@ -11,6 +11,27 @@ var page = (function(){
 	var bindHandler = function(){
 		$('#items .item').on('vclick', function(){
 			var target = $(this).attr('target');
+
+			if(target == 'photo'){
+
+				navigator.camera.getPicture(onSuccess, onFail, { 
+					quality: 50,
+    				destinationType: Camera.DestinationType.DATA_URL
+				});
+
+				function onSuccess(imageData) {
+				    var image = document.getElementById('myImage');
+				    image.src = "data:image/jpeg;base64," + imageData;
+				}
+
+				function onFail(message) {
+				    alert('Failed because: ' + message);
+				}
+
+				return false;
+			}
+
+
 			location.href='page/'+target+'.html';
 			
 			/*$.mobile.changePage( '../page/'+target+'.html', { 
